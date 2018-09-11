7d167238d67da7e4ae5bca15852da17ef86ac582
Jerry Chan <jerryctt@gmail.com>
2018-09-11 21:32:10 +0800

    Add decode filename argument

--- convert.py
+++ convert.py
@@ -2,19 +2,25 @@
 
 import io
 import json
+import sys
+
 from pogoprotos.networking.responses.download_item_templates_response_pb2 import DownloadItemTemplatesResponse
 from google.protobuf.json_format import MessageToDict
 
-def main():
-    with open('gm.bin', 'rb') as gm:
+def main(gmfile):
+    with open(gmfile, 'rb') as gm:
         msg = DownloadItemTemplatesResponse()
         msg.ParseFromString( gm.read() )
         dict_obj = MessageToDict(msg)
         #print json.dumps(dict_obj, sort_keys=True, indent=4)
-        with io.open('out.json', 'w', encoding='utf-8') as out:
+        with io.open( gmfile+'.json', 'w', encoding='utf-8') as out:
             out.write( unicode(json.dumps(dict_obj, sort_keys=True, indent=4)) )
             
     print "END"
         
 if __name__ == '__main__':
-    main()    
+    if len(sys.argv) != 2:
+        print('convert.py GAME_MASTER_FLIE_BIN')
+        sys.exit(0)
+
+    main(sys.argv[1])    
--- decode.py
+++ decode.py
@@ -7,7 +7,7 @@ import struct
 from base64 import *
 from Crypto.Cipher import AES
 
-def decrypt_file():
+def decrypt_file(fileKey, inFile, outFile):
     # File structure:
     #   VERSION (1 byte)
     #   IV (16 bytes)
@@ -15,7 +15,7 @@ def decrypt_file():
     #   TRAILER (20 bytes)
 
     key_mask = '\x50\x46\x41\x69\x24\x3B\x5D\x47\x37\x52\x67\x3E\x6B\x7A\x34\x77'
-    net_key = b64decode( 'YFe+Toiy77kuPV/5Broxzg==' )
+    net_key = b64decode( fileKey )
     key = ''
     for i in range(0, 16):
         key += chr(ord(net_key[i]) ^ ord(key_mask[i]))
@@ -31,7 +31,7 @@ def decrypt_file():
         # AES-128 => block of 128 bits (16 bytes)
         sys.stderr.write("Warning: unexpected size for encrypted data in %s\n" % path)
 
-    with open('d', 'rb') as input:
+    with open(inFile, 'rb') as input:
         version = struct.unpack('B', input.read(1))[0]
         if version != 1:
             sys.stderr.write("Error: invalid version number for %s (%d)\n" % (path, version))
@@ -44,11 +44,31 @@ def decrypt_file():
 
 
     # FIXME: sanitize bundle_name!
-    with open('a.txt', 'wb') as out:
+    with open(outFile, 'wb') as out:
         out.write(data)
     
     return True   
+    
+def readAdFile(adFileName):    
+    # Read MOVE name
+    with io.open(adFileName, encoding='utf-8') as aData:
+        ad = json.load( aData )
+        adItems = []
+        for item in ad:        
+            bundleName = item['bundleName']
+            if bundleName != 'i18n_moves' and bundleName != 'i18n_pokemon':
+                continue
+            adItems.append( item )
+        return adItems
         
+    return None
+    
 if __name__ == '__main__':
-    decrypt_file()    
+    if len(sys.argv) != 3:
+        print('decode.py [ad_json_file_name] [bundle_directory]')
+        sys.exit(0)
+    adItems = readAdFile(sys.argv[1])
+    
+    # Find the file
+    #decrypt_file()    
         
