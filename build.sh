1ebd5f9a3e408eeeb6f03d9fb8aacedb3856e5d6
Jerry Chan <jerryttchan@m800.com>
2019-10-25 13:54:01 +0800

    Add build script for pokemon game master parser

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/binsh
-mvn build
+rm -rf pokemongo-game-master
+git clone https://github.com/pokemongo-dev-contrib/pokemongo-game-master.git
+cd pokemongo-game-master
+libPath=`find ./lib/*.jar -type f`
+mvn install:install-file -Dfile="$libPath"
 mvn package
