e53b186d50024a17466ca82a689f731e754ebe12
Jerry Chan <jerryttchan@m800.com>
2020-01-02 09:58:46 +0800

    add shade plugin to pom.xml

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/binsh
 rm -rf pokemongo-game-master
 git clone https://github.com/pokemongo-dev-contrib/pokemongo-game-master.git
+patch pokemongo-game-master/pom.xml < add_shade_plugin.patch
 cd pokemongo-game-master
 libPath=`find ./lib/*.jar -type f`
 mvn install:install-file -Dfile="$libPath"
